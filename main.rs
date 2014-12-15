404989d6f8a9db89fb721065d907669e416dc006
Ap0ph1s <karimellaisy@gmail.com>
2014-12-15 12:13:36 -0500

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,13 +1,14 @@
 use std::io;
 
 fn main() {
-	println!("##########/// GPA Calculator \\\\\\############");
-	println!("Developed by Karim Ellaisy, Written in Rust\n\n *Note: this is based on Wyomissing's computing system(Simple GPA)\n\n Enter your grades for your classes in order: ");
+	println!("*******/// GPA Calculator \\\\\\*******");
+	println!("Developed by Karim Ellaisy, Written in Rust\n\n *Note: this is based on Wyomissing's computing system(Simple GPA)\n\n");
+	println!("Instructions: Type the uppercase letter grade then press enter\n enter `done` to calculate GPA")
 	let mut stdin = io::stdin();
-	let mut i = 1i;
-	let mut accum = 0i;
+	let mut i = 1f32;
+	let mut accum = 0f32;
     let mut reader = stdin.lock();
-    let (A,B,C,D,F) = (4i,3i,2i,1i,0i);
+    let (A,B,C,D,F) = (4f32,3f32,2f32,1f32,0f32);
 	for line in reader.lines() {
 		let srt = line.unwrap();
 		match srt.as_slice().trim() {
@@ -16,10 +17,9 @@ fn main() {
 			"C" => {accum += C; println!("Class: {}, GPA Points= {}", i, C)},
 			"D" => {accum += D; println!("Class: {}, GPA Points= {}", i, D)},
 			"F" => {accum += F; println!("Class: {}, GPA Points= {}", i, F)},
-			"done" =>{i += -1; println!("Calculated GPA: {}", accum / i)},
+			"done" =>{i += -1.00; println!("Calculated GPA: {}", accum / i)},
 			_ => println!("Input Invalid!"),
 		}
-		i += 1;
+		i += 1.00;
 	}
 }
-
